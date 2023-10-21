66be2390fd6b706c899368e7f875575b60362f47
kratoshakan <hakansozan.inci@gmail.com>
2023-10-21 07:16:15 +0300

    fix

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -39,9 +39,6 @@ wp user create $WP_USR $WP_EMAIL --role=author --user_pass=$WP_PWD --allow-root
 
 wp theme install astra --activate --allow-root
 
-
-wp plugin install redis-cache --activate --allow-root
-
 wp plugin update --all --allow-root
 
 
@@ -49,9 +46,5 @@ wp plugin update --all --allow-root
 sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf
 
 mkdir /run/php
-
-
-
-wp redis enable --allow-root
 
 /usr/sbin/php-fpm7.3 -F
