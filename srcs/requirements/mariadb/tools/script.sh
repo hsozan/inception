5acc424d234c1bf848b5816c8966aebb46d86983
kratoshakan <hakansozan.inci@gmail.com>
2023-10-21 07:29:31 +0300

    db fix

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -4,7 +4,7 @@ service mysql start
 
 
 echo "CREATE DATABASE IF NOT EXISTS $db_name ;" > db1.sql
-echo "CREATE USER IF NOT EXISTS '$db_user'@'%' IDENTIFIED BY '$db1_pwd' ;" >> db1.sql
+echo "CREATE USER IF NOT EXISTS '$db_user'@'%' IDENTIFIED BY '$db_pwd' ;" >> db1.sql
 echo "GRANT ALL PRIVILEGES ON $db_name.* TO '$db_user'@'%' ;" >> db1.sql
 echo "FLUSH PRIVILEGES;" >> db1.sql
 
