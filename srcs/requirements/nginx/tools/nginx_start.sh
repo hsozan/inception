74e5d0e0cc566a4232f7e2fd5c8bf04dea1df65e
kratoshakan <hakansozan.inci@gmail.com>
2023-10-21 08:57:16 +0300

    fix

--- srcs/requirements/nginx/tools/nginx_start.sh
+++ srcs/requirements/nginx/tools/nginx_start.sh
@@ -2,7 +2,7 @@
 
 if [ ! -f /etc/ssl/certs/nginx.crt ]; then
 echo "Nginx: setting up ssl ...";
-openssl req -x509 -nodes -days 365 -newkey rsa:4096 -keyout /etc/ssl/private/nginx.key -out /etc/ssl/certs/nginx.crt -subj "/C=Turkiye/ST=Kocaeli/L=Gebze/O=42Kocaeli/CN=hsozan.42.fr";
+openssl req -x509 -nodes -days 365 -newkey rsa:4096 -keyout /etc/ssl/private/nginx.key -out /etc/ssl/certs/nginx.crt -subj "/C=TR/ST=Kocaeli/L=Gebze/O=42Kocaeli/CN=hsozan.42.fr";
 echo "Nginx: ssl is set up!";
 fi
 
