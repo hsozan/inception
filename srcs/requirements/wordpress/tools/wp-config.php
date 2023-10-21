66bbf0bfc58d0a36960685ebb1e66ec23a8909e3
kratoshakan <hakansozan.inci@gmail.com>
2023-10-21 03:51:17 +0300

    ?

--- srcs/requirements/wordpress/tools/wp-config.php
+++ srcs/requirements/wordpress/tools/wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'db1' );
+define( 'DB_NAME', $db1_name );
 
 /** MySQL database username */
-define( 'DB_USER', 'user' );
+define( 'DB_USER', $db1_user );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'pwd' );
+define( 'DB_PASSWORD', $db1_pwd );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'mariadb' );
