86edd11e8069984c8f4f5a3d15d8f88d6d47e151
kratoshakan <hakansozan.inci@gmail.com>
2023-10-21 07:36:13 +0300

    db fix

--- srcs/requirements/wordpress/tools/wp-config.php
+++ srcs/requirements/wordpress/tools/wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', $db1_name );
+define( 'DB_NAME', $db_name );
 
 /** MySQL database username */
-define( 'DB_USER', $db1_user );
+define( 'DB_USER', $db_user );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', $db1_pwd );
+define( 'DB_PASSWORD', $db_pwd );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'mariadb' );
